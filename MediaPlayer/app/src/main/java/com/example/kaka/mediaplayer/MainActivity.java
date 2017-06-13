9319a63f8d8493027a34d0fc12156d987894277b
kaka <kaka@luka.com>
2017-06-14 00:31:03 +0900

    0614

--- MediaPlayer/app/src/main/java/com/example/kaka/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/kaka/mediaplayer/MainActivity.java
@@ -13,16 +13,20 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
 
     private final Handler httphandle = new Handler(){
@@ -34,29 +38,38 @@ public class MainActivity extends AppCompatActivity {
             {
                 case 0: //HTTP FAILED
 
+                    Toast.makeText(getApplicationContext(), "LIST LOAD FAILED", Toast.LENGTH_SHORT).show();
+                    Log.i("network", "NETWORK : HTTP CONNECT FAILED");
                     break;
 
                 case 1: //HTTP SUCCESS
 
+                    test_text.setText(builder.toString());
+                    Log.i("network", "NETWORK : HTTP CONNECT SUCCESS");
                     break;
             }
         }
     };
 
+    public StringBuilder builder;
+    public BufferedReader reader;
 
+    TextView test_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        test_text = (TextView) findViewById(R.id.test_text);
+
         // SERVER URL
-        String url = "http://108.61.200.205/";
+        String url = "http://108.61.200.205/pc_receiver.php";
         int param = 1;
         String message = "";
 
-        new CallHttp(url, param, message, httphandle);
-    }
+        new CallHttp(url, param, message, httphandle).start();
+}
 
 
     // HTTP CALL
@@ -96,16 +109,19 @@ public class MainActivity extends AppCompatActivity {
                 // INPUT STREAM OPEN
                 InputStream is = connection.getInputStream();
 
-                StringBuilder builder = new StringBuilder(); // STRING OBJECT
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8")); // STRING SETTING
+                builder = new StringBuilder(); // STRING OBJECT
+                reader = new BufferedReader(new InputStreamReader(is, "UTF-8")); // STRING SETTING
 
                 String line;
 
                 while((line = reader.readLine()) != null)
                 {
-
+                    builder.append(line);
                 }
 
+                message.what = 1;
+                message.arg1 = 1;
+                hand.sendMessage(message);
             }
             catch (Exception ex)
             {
@@ -114,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
                 hand.sendMessage(message);
 
                 Log.i("network", "CALL HTTP : " + ex.getMessage());
+                ex.printStackTrace();
             }
         }
     }
