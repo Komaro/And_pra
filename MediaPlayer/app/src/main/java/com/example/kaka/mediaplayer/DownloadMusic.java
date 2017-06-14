7d683534e2c6b8da845ec01df4960bbc94dc98f1
komaro <komaro318@gmail.com>
2017-06-14 12:56:50 +0900

    0614

---
+++ MediaPlayer/app/src/main/java/com/example/kaka/mediaplayer/DownloadMusic.java
@@ -0,0 +1,113 @@
+package com.example.kaka.mediaplayer;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.Externalizable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Administrator on 2017-06-14.
+ */
+
+public class DownloadMusic extends AsyncTask {
+
+    String title;
+    URL send_url;
+    Handler hand;
+    int position;
+
+
+    protected void onPreExecute() {
+
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+
+        Message message = hand.obtainMessage();
+        message.what = 2;
+        message.arg1 = position;
+
+        hand.sendMessage(message);
+
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+
+        this.title = params[0].toString();
+        this.hand = (Handler)params[1];
+        this.position = (int)params[2];
+        String path = params[3].toString();
+
+        // HttpClient DEPRECATE
+        // REFERENCE : http://mommoo.tistory.com/5
+        try {
+
+            String encode_title = URLEncoder.encode(title, "UTF-8");
+            encode_title = encode_title.replace("+", "%20");
+
+            String url = "http://108.61.200.205/MusicList/" + encode_title + ".mp3";
+            Log.i("title", "TITLE : " + encode_title + ".mp3");
+
+            send_url = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) send_url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(false); // WRITING
+            connection.setDoInput(true); // READING
+            connection.setUseCaches(false);
+            connection.setDefaultUseCaches(false);
+            connection.setConnectTimeout(10000);
+
+
+            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                File file = new File(path + title + ".mp3");
+                FileOutputStream fileoutput = new FileOutputStream(file);
+                InputStream inputstream = connection.getInputStream();
+
+                int downloadedSize = 0;
+                byte[] buffer = new byte[1024];
+                int bufferLength = 0;
+                int total = connection.getContentLength();
+
+                while((bufferLength = inputstream.read(buffer)) > 0)
+                {
+                    fileoutput.write(buffer, 0 , bufferLength);
+                    downloadedSize += bufferLength;
+                    publishProgress((int)(downloadedSize * 100) / total);
+                }
+                fileoutput.close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.i("download", "DOWNLOAD : " + ex.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+
+        Message message = hand.obtainMessage();
+        message.what = 3;
+        message.arg1 = (int)values[0];
+        hand.sendMessage(message);
+    }
+}
--- MediaPlayer/app/src/main/java/com/example/kaka/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/kaka/mediaplayer/MainActivity.java
@@ -1,20 +1,23 @@
 /*
     //TAG
     network
-
-
-
+    title
  */
 
 package com.example.kaka.mediaplayer;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -22,14 +25,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private final Handler httphandle = new Handler(){
+    public final Handler httphandle = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -44,8 +45,32 @@ public class MainActivity extends AppCompatActivity {
 
                 case 1: //HTTP SUCCESS
 
-                    test_text.setText(builder.toString());
+                    catch_string = builder.toString();
+                    catch_list = catch_string.split("┃");
+
+                    ArrayAdapter<String> list_output = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, catch_list);
+                    playlist.setAdapter(list_output);
+
                     Log.i("network", "NETWORK : HTTP CONNECT SUCCESS");
+                    break;
+
+                case 2 : //DOWNLOAD COMPLETE
+
+                    try {
+                        music_player.reset();
+                        music_player.setDataSource(getCacheDir() + catch_list[msg.arg1] + ".mp3");
+                        music_player.prepare();
+                        music_player.start();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                case 3: // PROGRESSBAR REFRESH
+
+                    download_pro.setProgress(msg.arg1);
                     break;
             }
         }
@@ -54,37 +79,50 @@ public class MainActivity extends AppCompatActivity {
     public StringBuilder builder;
     public BufferedReader reader;
 
-    TextView test_text;
+    ListView playlist;
+    String catch_string;
+    String[] catch_list;
+
+    private MediaPlayer music_player;
+    private ProgressBar download_pro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        test_text = (TextView) findViewById(R.id.test_text);
+        playlist = (ListView)findViewById(R.id.playlist);
+        download_pro = (ProgressBar)findViewById(R.id.download_pro);
+        music_player = new MediaPlayer();
+        music_player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        출처: http://unikys.tistory.com/350 [All-round programmer]
+
+        playlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                new DownloadMusic().execute(catch_list[position], httphandle, position, getCacheDir());
+            }
+        });
 
         // SERVER URL
-        String url = "http://108.61.200.205/pc_receiver.php";
+        String url = "http://108.61.200.205/request_playlist.php";
         int param = 1;
         String message = "";
 
-        new CallHttp(url, param, message, httphandle).start();
+        new CallHttp(url, httphandle).start();
 }
-
 
     // HTTP CALL
     private class CallHttp extends Thread {
 
-        private String url, msg;
-        private int para;
+        private String url;
         private Handler hand;
 
-
-        public CallHttp(String url, int para, String msg, Handler hand)
+        public CallHttp(String url,  Handler hand)
         {
             this.url = url;
-            this.para = para;
-            this.msg = msg;
             this.hand = hand;
         }
 
@@ -120,13 +158,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 message.what = 1;
-                message.arg1 = 1;
                 hand.sendMessage(message);
             }
             catch (Exception ex)
             {
                 message.what = 0;
-                message.arg1 = 0;
                 hand.sendMessage(message);
 
                 Log.i("network", "CALL HTTP : " + ex.getMessage());
